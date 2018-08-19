fc2dedbb6e7d1bf7b9694bdaf2baad388f63e779
Piyush Chaudhari <Piyush.Chaudhari@futureretail.in>
2018-08-19 14:44:32 +0530

    create account service

---
+++ src/main/java/create-account/src/main/java/com/account/service/CreateAccountApplication.java
@@ -0,0 +1,12 @@
+package com.account.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateAccountApplication.class, args);
+	}
+}
