ba0124f74daf191c7149d532627576d35e419f83
Piyush Chaudhari <Piyush.Chaudhari@futureretail.in>
2018-08-19 15:02:58 +0530

    added account-create-service

---
+++ src/main/java/account-create-service/src/main/java/com/account/create/AccountCreateServiceApplication.java
@@ -0,0 +1,12 @@
+package com.account.create;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountCreateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountCreateServiceApplication.class, args);
+	}
+}
---
+++ src/main/java/account-create-service/src/test/java/com/account/create/AccountCreateServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.account.create;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccountCreateServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
