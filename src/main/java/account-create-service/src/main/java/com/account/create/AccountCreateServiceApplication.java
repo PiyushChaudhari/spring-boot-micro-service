e43f56cf350b58fa26970d4c5af8d3f2ee9f4dd6
Piyush Chaudhari <Piyush.Chaudhari@futureretail.in>
2018-08-19 17:08:35 +0530

    Register Account Create Service To Eureka Server

--- src/main/java/account-create-service/src/main/java/com/account/create/AccountCreateServiceApplication.java
+++ src/main/java/account-create-service/src/main/java/com/account/create/AccountCreateServiceApplication.java
@@ -2,8 +2,10 @@ package com.account.create;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AccountCreateServiceApplication {
 
 	public static void main(String[] args) {
