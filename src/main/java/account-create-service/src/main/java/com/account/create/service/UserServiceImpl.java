0f8cf6b2b4158bc9592b9150bd2825fea666f1ef
Piyush Chaudhari <Piyush.Chaudhari@futureretail.in>
2018-08-19 20:41:42 +0530

    Added below changes.
    1) call notification-service from account-create-service
    
    2) update zuul gateway configuration.

--- src/main/java/account-create-service/src/main/java/com/account/create/service/UserServiceImpl.java
+++ src/main/java/account-create-service/src/main/java/com/account/create/service/UserServiceImpl.java
@@ -1,9 +1,16 @@
 package com.account.create.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.account.create.dao.UserDao;
 import com.account.create.model.User;
@@ -15,11 +22,36 @@ public class UserServiceImpl implements UserService{
 	
 	@Autowired
 	private UserDao userDao;
+	
+	@Autowired
+	private DiscoveryClient discoveryClient;
 
 	@Override
 	public User save(User user) {
 		log.info("save user:> {} ",user);
-		return userDao.save(user);
+		User u = userDao.save(user);
+		if(null !=u) {
+			sendNotification(u);
+		}
+		return u;
+	}
+	
+	private void sendNotification(User u) {
+		List<ServiceInstance> instances = discoveryClient.getInstances("notification-service");
+		ServiceInstance serviceInstance = instances.get(0);
+
+		StringBuilder baseUrl = new StringBuilder(serviceInstance.getUri().toString());
+		baseUrl.append("/notification/registration/welcome");
+		
+		Map<String,String> modelMap = new HashMap<>();
+		
+		modelMap.put("to",u.getEmail());
+		modelMap.put("name",u.getFirstName());
+		modelMap.put("signature","Notification Team");
+		modelMap.put("location","Ahmedabad");
+		RestTemplate restTemplate = new RestTemplate();
+		String result = restTemplate.postForObject(baseUrl.toString(), modelMap, String.class);
+		log.info("result:> {} ",result);
 	}
 
 }
